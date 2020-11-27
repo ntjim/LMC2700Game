55f203c9550d301767f860747836508e8b15bcf5
Arthur Clay Odom <thekingofthebritons@gmail.com>
2020-11-27 18:33:33 -0500

    Finished entity module skeleton

--- entity.js
+++ entity.js
@@ -15,4 +15,21 @@ function Meteor(x, y, fallSpeed) {
     this.fallSpeed = fallSpeed;
 }
 
-export {Entity};
+function Enemy(x, y, health) {
+    Entity.call(this, x, y);
+    this.health = health;
+}
+
+function Player(x, y) {
+    Entity.call(this, x, y);
+    this.update = function (input) {return true;};
+    this.shoot = function () { }; //TODO: Shooting Functionality
+    this.dive = function () { }; //TODO: Diving 
+}
+
+function Bullet(x, y, speed) {
+    Entity.call(this, x, y);
+    this.update = function () {return true;} //TODO: Bullet Movement
+}
+
+export {Entity, Meteor, Enemy};
--- game.js
+++ game.js
@@ -6,15 +6,15 @@ function Game(Input, graphicsFrontend, player, meteorSpawner, enemySpawner, enti
     this.entityList = entityList;
     
     this.update = function() {
-        return false;
+        return false; //TODO: The Game
     }
     
     this.gameOver = function() {
-        //Void Function
+        //Void Function //TODO: Setup GameOver functionality; Return Highscores, etc.
     }
     
     this.checkCollisions = function() {
-        //Void Function
+        //Void Function //TODO: create a collision checking function to run every frame.
     }
 }
 
